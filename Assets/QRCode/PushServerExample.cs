1d9b7212a1b2a9783a4d7b90517dd947e126241f
huafeng <ai29027623@vip.qq.com>
2025-02-20 11:31:25 +0800

    Init git

---
+++ Assets/QRCode/PushServerExample.cs
@@ -0,0 +1,355 @@
+using OpenCover.Framework.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+using System.Xml;
+using System.IO;
+using UnityEngine.UI;
+
+namespace OA 
+{
+    public class PushServerExample : MonoBehaviour
+    {
+        public RawImage QRImage;
+
+
+        [Header("注意，正常屏幕下texture的贴图尺寸如果过大，需要调小至64/128")]
+        public Texture2D QRTexture;
+        public int resultCode;
+
+        /// <summary>
+        /// 请求licid的协程
+        /// </summary>
+        private IEnumerator RequestLicIdIenumerator;
+
+        /// <summary>
+        /// licid轮询激活状态的协程
+        /// </summary>
+        private IEnumerator GetLicIdStateIenurator;
+
+
+
+        public string serverUrl = "https://apifoxmock.com/m1/5105109-0-default/api/license";
+
+        public string getActiveStateUrl = "https://apifoxmock.com/m1/5105109-0-default/api/license?Id=";
+        private void Start()
+        {
+            RefreshQR();
+        }
+        public void RefreshQR()
+        {
+
+            OAAppsettings oAAppsettings = new OAAppsettings(Path.Combine(Environment.CurrentDirectory, "OAApp.exe.config"));
+            var json = JsonConvert.SerializeObject(oAAppsettings);
+
+
+            RequestLicIdIenumerator=PostGameDataMsg(serverUrl, json, PushDataCallback);
+            GetLicIdStateIenurator= GetActiveStateFromServer(getActiveStateUrl, resultCode, GetActiveStateCallback);
+
+
+            StartCoroutine(RequestLicIdIenumerator);
+        }
+
+        /// <summary>
+        /// 根据当前xml文件，请求QR码数据
+        /// </summary>
+        /// <param name="_url">请求的地址</param>
+        /// <param name="jsonString">当前项目的配置信息</param>
+        /// <param name="ac">回调事件</param>
+        /// <returns></returns>
+        public static IEnumerator PostGameDataMsg(string _url, string jsonString, Action<string> ac)
+        {
+            Regex reg = new Regex(@"(?i)\\[uU]([0-9a-f]{4})");
+            string s = reg.Replace(jsonString, delegate (Match m) { return ((char)Convert.ToInt32(m.Groups[1].Value, 16)).ToString(); });
+            // string url = string.Format("http://{0}/Base_Manage/Base_Train/SaveData", _url);
+            //print("url:" + url);
+            //  print("*************************************************");
+            // print("Json:" + s);
+            // print("url:" + _url);
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(s);
+            UnityWebRequest request = new UnityWebRequest(_url, "POST");
+            request.uploadHandler = new UploadHandlerRaw(bytes);
+            request.certificateHandler = new WebReqSkipCert();
+            DownloadHandlerBuffer dH = new DownloadHandlerBuffer();
+            request.useHttpContinue = false;
+            request.downloadHandler = dH;
+            request.SetRequestHeader("Content-Type", "application/json;charset=utf-8");
+            // request.SetRequestHeader("Authorization", "Bearer " + SystemDefine.Instances.Data);
+            // request.SetRequestHeader("Loginversion",);
+            yield return request.SendWebRequest();
+            if (request.isHttpError || request.isNetworkError)
+            {
+                Debug.Log("上传数据失败！");
+                //Debug.Log(request.error);
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log("上传数据成功！");
+                //print(request.downloadHandler.text);
+                if (ac != null)
+                {
+                    ac.Invoke(request.downloadHandler.text);
+                }
+
+            }
+        }
+
+
+        public static IEnumerator GetActiveStateFromServer(string _url,int licId,Action<string> ac)
+        {
+            // 定义请求的URL
+            string TargetUrl = _url + licId;
+
+            // 创建一个UnityWebRequest对象，使用GET方法
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(TargetUrl))
+            {
+                // 发送请求并等待响应
+                yield return webRequest.SendWebRequest();
+
+                // 检查请求的结果
+                if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    // 如果发生错误，打印错误信息
+                    Debug.LogError("Error: " + webRequest.error);
+                }
+                else
+                {
+                    // 如果请求成功，打印响应内容
+                    ac?.Invoke(webRequest.downloadHandler.text);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 提交应用、设备信息、换取licid事件的回调
+        /// </summary>
+        /// <param name="ServerData"></param>
+        private void PushDataCallback(string ServerData) 
+        {
+            var data= JsonConvert.DeserializeObject<RequestServerResponseData>(ServerData);
+            Debug.Log(ServerData);
+            var QRCode = data.result;
+            resultCode = Convert.ToInt32(QRCode); 
+            ZXingQRCodeWrapper_GenerateQRCode.SetRawImageQRCode(QRImage, QRTexture, QRCode, Color.blue);
+
+
+            //获取到licid后，开始轮询激活状态
+            StartCoroutine(GetLicIdStateIenurator);
+        }
+
+
+        /// <summary>
+        /// 获取licid激活状态的回调
+        /// </summary>
+        /// <param name="jsonData"></param>
+        private void GetActiveStateCallback(string jsonData) 
+        {
+            var data = JsonConvert.DeserializeObject<YGXJ_QRActiveData>(jsonData);
+            Debug.Log(jsonData);
+            if (data.result.activated)
+            {
+                Debug.Log("激活成功");
+            }
+            else
+            {
+                Debug.Log("激活失败");
+            }
+        }
+    }
+
+    #region   请求QR码的数据类
+    public class OAAppsettings 
+    {
+        private Dictionary<string, string> dict;
+        public OAAppsettings(string path)
+        {
+            this.dict = new Dictionary<string, string>();
+            string filename = path;
+            XmlDocument xmldoc = new XmlDocument();
+
+
+            xmldoc.Load(filename);
+            XmlNodeList topM = xmldoc.DocumentElement.ChildNodes;
+            foreach (XmlElement element in topM)
+            {
+
+                if (element.Name.ToLower() == "appsettings")
+                {
+                    XmlNodeList nodelist = element.ChildNodes;
+                    if (nodelist.Count > 0)
+                    {
+                        foreach (XmlElement el in nodelist)
+                        {
+                            if (!dict.ContainsKey(el.Attributes["key"].Value))
+                            {
+                                dict.Add(el.Attributes["key"].Value, el.Attributes["value"].Value);
+                            }
+                            else
+                            {
+                                dict[el.Attributes["key"].Value] = el.Attributes["value"].Value;
+                            }
+                        }
+                    }
+                }
+
+
+            }
+            xmldoc.Save(filename);
+        }
+        private void Add(string key, string value)
+        {
+            if (!this.dict.ContainsKey(key))
+            {
+                this.dict.Add(key, value);
+            }
+            else
+            {
+                this.dict[key] = value;
+            }
+        }
+        public string this[string key]
+        {
+            get
+            {
+                if (!this.dict.ContainsKey(key))
+                {
+                    throw new Exception("404");
+                }
+                return this.dict[key];
+            }
+            set
+            {
+                if (this.dict.ContainsKey(key))
+                {
+                    this.dict[key] = value;
+                }
+                else
+                {
+                    this.dict.Add(key, value);
+                }
+            }
+        }
+
+        // 添加一个方法用于获取 AppSettings 数据
+        // 添加一个方法用于获取 AppSettings 数据
+        public YGXJ_OARequestQRDate.AppSettings[] GetAppSettings()
+        {
+            if (dict.ContainsKey("appSettings"))
+            {
+                string appSettingsJson = dict["appSettings"];
+                // 去除多余的转义字符
+                appSettingsJson = appSettingsJson.Replace("\\\"", "\"");
+
+                try
+                {
+                    return JsonConvert.DeserializeObject<YGXJ_OARequestQRDate.AppSettings[]>(appSettingsJson);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"反序列化 AppSettings 时出错: {ex.Message}");
+                }
+            }
+            return null;
+        }
+    }
+
+    public class YGXJ_OARequestQRDate
+    {
+        public string appName;
+        public string appCode;
+        public string appDesc;
+        public string installPath;
+        public string appVersion;
+        public AppSettings[] appSettings;
+        public Device device;
+
+        public class AppSettings
+        {
+            public string groupName;
+            public string label;
+            public string labelText;
+            public string labelDesc;
+            public string value;
+
+            public enum UiType
+            {
+                Input = 0,
+                Switch = 1,
+                Select = 2,
+                Radio = 3,
+                Checkbox = 4,
+            }
+
+            public UiType uiType;
+
+            public string placeholder;
+
+            public Dictionary<string, string> data;
+
+
+        }
+        public class Device
+        {
+            public string ip;
+            public string hostName;
+            public string machine;
+            public string osVersion;
+        }
+    }
+
+    /// <summary>
+    /// 请求二维码数据时，服务器返回的所有数据
+    /// </summary>
+    public class RequestServerResponseData
+    {
+        public int code;
+        public string type;
+        public string message;
+        public string result;
+        public string extras;
+        public string time;
+    }
+
+    #endregion
+
+
+
+    #region LicID轮询请求的数据类
+
+    public class YGXJ_QRActiveData
+    {
+        public string code;
+        public string type;
+        public string message;
+        public Active_Result result;
+
+        public string extras;
+        public string time;
+        public class Active_Result 
+        {
+            public int id;
+            public string signature; 
+            public string appName;
+            public string appCode;
+            public string appVersion;
+            public string appDesc;
+            public int appSettingsVer;
+            public YGXJ_OARequestQRDate.AppSettings[] appSettings;
+            public string installPath;
+            public string orderId;
+            public int deviceId;
+            public bool activated;
+            public string activationTime;
+            public string updateTime;
+            public int userId;
+        }
+
+
+    }
+    #endregion
+}
---
+++ Assets/QRCode/ZXingQRCodeWrapper_GenerateQRCode.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.Common;
+
+/// <summary>
+/// 生成二维码的工具类
+/// </summary>
+public  static class ZXingQRCodeWrapper_GenerateQRCode 
+{
+
+    /// <summary>
+    /// 设置一个二维码内容
+    /// </summary>
+    /// <param name="img">rowImage对象</param>
+    /// <param name="texture">二维码中心图标</param>
+    /// <param name="Content">二维码实际内容</param>
+    /// <param name="color">二维码颜色</param>
+    /// <param name="width">二维码宽度</param>
+    /// <param name="height">二维码高度</param>
+    public static void SetRawImageQRCode(RawImage img, Texture2D texture, string Content,Color color, int width = 290, int height = 290)
+    {
+        img.texture= GenerateQRImageWithColorAndIcon(Content, width, height, color, texture);
+    }
+    /// <summary>
+    /// 生成2维码 方法一
+    /// 经测试：只能生成256x256的
+    /// </summary>
+    /// <param name="content"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    static Texture2D GenerateQRImage1(string content, int width, int height)
+    {
+        // 编码成color32
+        EncodingOptions options = null;
+        BarcodeWriter writer = new BarcodeWriter();
+        options = new EncodingOptions
+        {
+            Width = width,
+            Height = height,
+            Margin = 1,
+        };
+        options.Hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+        writer.Format = BarcodeFormat.QR_CODE;
+        writer.Options = options;
+        Color32[] colors = writer.Write(content);
+
+        // 转成texture2d
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels32(colors);
+        texture.Apply();
+
+        //存储成文件
+        //byte[] bytes = texture.EncodeToPNG();
+        //string path = System.IO.Path.Combine(Application.dataPath, "qr.png");
+        //System.IO.File.WriteAllBytes(path, bytes);
+
+        return texture;
+    }
+
+    /// <summary>
+    /// 生成2维码 方法二
+    /// 经测试：能生成任意尺寸的正方形
+    /// </summary>
+    /// <param name="content"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    static Texture2D GenerateQRImageWithColor(string content, int width, int height, Color color)
+    {
+        BitMatrix bitMatrix;
+        Texture2D texture = GenerateQRImageWithColor(content, width, height, color, out bitMatrix);
+        return texture;
+    }
+
+    /// <summary>
+    /// 生成2维码 方法二
+    /// 经测试：能生成任意尺寸的正方形
+    /// </summary>
+    /// <param name="content"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    static Texture2D GenerateQRImageWithColor(string content, int width, int height, Color color, out BitMatrix bitMatrix)
+    {
+        // 编码成color32
+        MultiFormatWriter writer = new MultiFormatWriter();
+        Dictionary<EncodeHintType, object> hints = new Dictionary<EncodeHintType, object>();
+        //设置字符串转换格式，确保字符串信息保持正确
+        hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+        // 设置二维码边缘留白宽度（值越大留白宽度大，二维码就减小）
+        hints.Add(EncodeHintType.MARGIN, 1);
+        hints.Add(EncodeHintType.ERROR_CORRECTION, ZXing.QrCode.Internal.ErrorCorrectionLevel.M);
+        //实例化字符串绘制二维码工具
+        bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, width, height, hints);
+
+        // 转成texture2d
+        int w = bitMatrix.Width;
+        int h = bitMatrix.Height;
+        Debug.Log(string.Format("w={0},h={1}", w, h));
+        Texture2D texture = new Texture2D(w, h);
+        for (int x = 0; x < h; x++)
+        {
+            for (int y = 0; y < w; y++)
+            {
+                if (bitMatrix[x, y])
+                {
+                    texture.SetPixel(y, x, color);
+                }
+                else
+                {
+                    texture.SetPixel(y, x, Color.white);
+                }
+            }
+        }
+        texture.Apply();
+
+        //存储成文件
+        //byte[] bytes = texture.EncodeToPNG();
+        //string path = System.IO.Path.Combine(Application.dataPath, "qr.png");
+        //System.IO.File.WriteAllBytes(path, bytes); 
+        return texture;
+    }
+
+    /// <summary>
+    /// 生成2维码 方法三
+    /// 在方法二的基础上，添加小图标
+    /// </summary>
+    /// <param name="content"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <returns></returns>
+    static Texture2D GenerateQRImageWithColorAndIcon(string content, int width, int height, Color color, Texture2D centerIcon)
+    {
+        BitMatrix bitMatrix;
+        Texture2D texture = GenerateQRImageWithColor(content, width, height, color, out bitMatrix);
+        int w = bitMatrix.Width;
+        int h = bitMatrix.Height;
+        // 添加小图
+        int halfWidth = texture.width / 2;
+        int halfHeight = texture.height / 2;
+        int halfWidthOfIcon = centerIcon.width / 2;
+        int halfHeightOfIcon = centerIcon.height / 2;
+        int centerOffsetX = 0;
+        int centerOffsetY = 0;
+        for (int x = 0; x < h; x++)
+        {
+            for (int y = 0; y < w; y++)
+            {
+                centerOffsetX = x - halfWidth;
+                centerOffsetY = y - halfHeight;
+                if (Mathf.Abs(centerOffsetX) <= halfWidthOfIcon && Mathf.Abs(centerOffsetY) <= halfHeightOfIcon)
+                {
+                    texture.SetPixel(x, y, centerIcon.GetPixel(centerOffsetX + halfWidthOfIcon, centerOffsetY + halfHeightOfIcon));
+                }
+            }
+        }
+        texture.Apply();
+        // 存储成文件
+        byte[] bytes = texture.EncodeToPNG();
+        string path = System.IO.Path.Combine(Application.dataPath, "qr.png");
+        Debug.Log(path);
+        System.IO.File.WriteAllBytes(path, bytes);
+        return texture;
+    }
+}
